a9a141210f90f8b5a5a27a51cd5d61cff5c82235
yasinsz <yasin.sz@hotmail.com>
2020-05-04 18:36:14 +0200

    footer icons

--- resources/views/frontend/home.blade.php
+++ resources/views/frontend/home.blade.php
@@ -2,27 +2,22 @@
 
 @section('content-home')
 
-
-<main>
-
-    <div class="container">
-        <div class="card">
-            <img src="https://source.unsplash.com/category/nature/330x200">
-            <h2>Movie Title 1</h2>
-            <p>Year: 2003</p>
-            <p>Genre: Horror</p>
-            <p>Rating: 4/5</p>
-        </div>
-
-        <div class="card">
-            <img src="https://source.unsplash.com/category/nature/330x200">
-            <h2>Movie Title 2</h2>
-            <p>Year: 2003</p>
-            <p>Genre: Horror</p>
-            <p>Rating: 4/5</p>
-        </div>
+<div class="container">
+    <div class="card">
+        <img src="https://source.unsplash.com/category/nature/330x200">
+        <h2>Movie Title 1</h2>
+        <p>Year: 2003</p>
+        <p>Genre: Horror</p>
+        <p>Rating: 4/5</p>
     </div>
 
-</main>
+    <div class="card">
+        <img src="https://source.unsplash.com/category/nature/330x200">
+        <h2>Movie Title 2</h2>
+        <p>Year: 2003</p>
+        <p>Genre: Horror</p>
+        <p>Rating: 4/5</p>
+    </div>
+</div>
 
 @endsection
--- resources/views/frontend/includes/footer.blade.php
+++ resources/views/frontend/includes/footer.blade.php
@@ -1,15 +1,13 @@
-<footer>
-
     <section class="footer-section border-radius">
         <div class="footer">
+
             <a href="{{ url('/') }}">
+                <img class="footer-item" src="https://image.flaticon.com/icons/svg/846/846449.svg">
 
-                <img class="footer-item" src="../Desktop/icon/home.svg">
-                <a href="{{ url('/') }}">
-                    <img class="footer-item" src="../Desktop/icon/search.svg">
-                    <a href="{{ url('/search') }}">
-                        <img class="footer-item" src="../Desktop/icon/card.svg">
-                        <a href="{{ url('/create') }}">
+                <a href="{{ url('/search') }}">
+                    <img class="footer-item" src="https://image.flaticon.com/icons/svg/483/483356.svg">
+
+                    <a href="{{ url('/create') }}">
+                        <img class="footer-item" src="https://image.flaticon.com/icons/svg/1250/1250616.svg">
+
     </section>
-
-</footer>
