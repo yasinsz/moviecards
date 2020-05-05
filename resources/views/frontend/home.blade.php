31a8574cc649aeb0c9e76ac1f2a28d9c467b097c
yasinsz <yasin.sz@hotmail.com>
2020-05-05 10:53:49 +0200

    add cards

--- resources/views/frontend/home.blade.php
+++ resources/views/frontend/home.blade.php
@@ -18,6 +18,15 @@
         <p>Genre: Horror</p>
         <p>Rating: 4/5</p>
     </div>
+
+    <div class="card">
+        <img src="https://source.unsplash.com/category/nature/330x200">
+        <h2>Movie Title 2</h2>
+        <p>Year: 2003</p>
+        <p>Genre: Horror</p>
+        <p>Rating: 4/5</p>
+    </div>
+
 </div>
 
 @endsection
