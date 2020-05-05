f8373e24b543a86940d6c777c8b04eb5dceb2a29
yasinsz <yasin.sz@hotmail.com>
2020-05-05 10:57:44 +0200

    cards

--- resources/views/frontend/home.blade.php
+++ resources/views/frontend/home.blade.php
@@ -21,7 +21,7 @@
 
     <div class="card">
         <img src="https://source.unsplash.com/category/nature/330x200">
-        <h2>Movie Title 2</h2>
+        <h2>Movie Title 3</h2>
         <p>Year: 2003</p>
         <p>Genre: Horror</p>
         <p>Rating: 4/5</p>
