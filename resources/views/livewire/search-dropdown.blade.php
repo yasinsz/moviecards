8be240ef4da78a85ba7afea102f831b13344302b
yasinsz <yasin.sz@hotmail.com>
2020-05-11 14:14:48 +0200

    styled movie api

--- resources/views/livewire/search-dropdown.blade.php
+++ resources/views/livewire/search-dropdown.blade.php
@@ -1,29 +1,37 @@
-<div class="relative d-flex justify-content-center">
-    <input wire:model.debounce.500ms="search" type="text" class="bg-danger rounded-full" width="64px" height="30px" placeholder="Search...">
-</div>
+<div class="container-dropdown">
 
-@if (strlen($search) >= 2)
-<div class="z-50 absolute bg-gray-800 text-sm rounded w-64 mt-4">
+    <div class="row-dropdown">
 
-    @if ($searchResults->count() > 0)
-    <ul>
-        @foreach ($searchResults as $result)
-        <li class="border-b border-gray-700">
-            <a href="{{ route('show', $result['id']) }}" class="block hover:bg-gray-700 px-3 py-3 flex items-center">
+        <div class="relative d-flex justify-content-center">
+            <input wire:model.debounce.500ms="search" type="text" class="bg-danger rounded-full" width="64px" height="30px" placeholder="Search...">
 
-                @if ($result['poster_path'])
-                <img src="https://image.tmdb.org/t/p/w92/{{ $result['poster_path'] }}" alt="poster" class="w-8">
+
+            @if (strlen($search) >= 2)
+            <div class="dropdown" width="64px" height="30px">
+
+                @if ($searchResults->count() > 0)
+                <ul class=" drop-list">
+                    @foreach ($searchResults as $result)
+                    <li class="drop-items">
+                        <a href=" {{ route('show', $result['id']) }}">
+
+                            @if ($result['poster_path'])
+                            <img src="https://image.tmdb.org/t/p/w92/{{ $result['poster_path'] }}" alt="poster" width="40px">
+                            @else
+                            <img src="https://via.placeholder.com/50x75" alt="poster" width="40px">
+                            @endif
+                            <span>{{ $result['title'] }}</span>
+                        </a>
+                    </li>
+                    @endforeach
+
+                </ul>
                 @else
-                <img src="https://via.placeholder.com/50x75" alt="poster" class="w-8">
+                <div> No results for "{{ $search }}"</div>
                 @endif
-                <span class="ml-4">{{ $result['title'] }}</span>
-            </a>
-        </li>
-        @endforeach
+            </div>
+            @endif
+        </div>
+    </div>
 
-    </ul>
-    @else
-    <div class="px-3 py-3">No results for "{{ $search }}"</div>
-    @endif
 </div>
-@endif
