f7f6e5fbc096341962d71c26c51a01e860dfacf7
yasinsz <yasin.sz@hotmail.com>
2020-05-12 14:07:43 +0200

    new card design

--- resources/views/frontend/home.blade.php
+++ resources/views/frontend/home.blade.php
@@ -2,34 +2,19 @@
 
 @section('content')
 
-<div class="container">
+<div class="card-container">
     <div class="card">
-        <span class="card__bookmark card__bookmark--active"></span>
-        <img src="https://images-na.ssl-images-amazon.com/images/I/71wbalyU7tL._AC_SL1481_.jpg" width="300px" height="220px">
-        <h2>Joker</h2>
-        <p>Year: 2019</p>
-        <p>Genre: Drama/Krimi</p>
-        <p>IMDB Rating: 8,1/10</p>
+        <span class="card-bookmark"></span>
+        <img class="card-poster" src="https://images-na.ssl-images-amazon.com/images/I/71wbalyU7tL._AC_SL1481_.jpg"></img>
     </div>
-
-    <div class="card">
-        <span class="card__bookmark card__bookmark--active"></span>
-        <img src="https://i.pinimg.com/originals/fd/5e/66/fd5e662dce1a3a8cd192a5952fa64f02.jpg" width="300px" height="220px">
-        <h2>Fight Club</h2>
-        <p>Year: 1999</p>
-        <p>Genre: Drama/Thriller</p>
-        <p>IMDB Rating: 8,8/10</p>
+    <div>
+        <a class="card-title">Joker</a>
+        <ul class="card-info">
+            <li>Rating: 8/10</li>
+            <li>2019</li>
+            <li>Drama/Krimi</li>
+        </ul>
     </div>
-
-    <div class="card">
-        <span class="card__bookmark card__bookmark--active"></span>
-        <img src="https://images-na.ssl-images-amazon.com/images/I/81iwrVUc5GL._AC_SY741_.jpg" width="300px" height="220px">
-        <h2>Parasite</h2>
-        <p>Year: 2019</p>
-        <p>Genre: Thriller/Komödie</p>
-        <p>IMDB Rating: 8,6/10</p>
-    </div>
-
 </div>
 
 @endsection
