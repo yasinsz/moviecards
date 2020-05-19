ec988d9a9dc6d37fb4dd328c33b01ce9a8ca6694
yasinsz <yasin.sz@hotmail.com>
2020-05-19 15:45:40 +0200

    add star svg

--- resources/views/frontend/home.blade.php
+++ resources/views/frontend/home.blade.php
@@ -10,7 +10,9 @@
         <img class="card-poster" src="{{ $card->poster_path }}"></img>
         <a class="card-title">{{ $card->title }}</a>
         <ul class="card-info">
-            <li>{{ $card->vote_average }}</li>
+            <li><svg class="star-svg" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="yellow" width="18px" height="18px">
+                    <path d="M12 17.27L18.18 21l-1.64-7.03L22 9.24l-7.19-.61L12 2 9.19 8.63 2 9.24l5.46 4.73L5.82 21z" />
+                    <path d="M0 0h24v24H0z" fill="none" /></svg>{{ $card->vote_average }}</li>
             <li>{{ $card->release_date }}</li>
             <li>{{ $card->name }}</li>
         </ul>
