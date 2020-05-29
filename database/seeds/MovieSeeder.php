3f3fbece275e03d5c75a81741016da21cb707abd
yasinsz <yasin.sz@hotmail.com>
2020-05-29 11:19:23 +0200

    fix seeder

--- database/seeds/MovieSeeder.php
+++ database/seeds/MovieSeeder.php
@@ -12,19 +12,19 @@ class MovieSeeder extends Seeder
      */
     public function run()
     {
-        DB::table('movies')->insert(
-            [
-                'title' => 'ES',
-                'rating' => '8',
-                'date' => '2019',
-                'genre' => 'Action'
-            ],
-            [
-                'title' => 'ES',
-                'rating' => '8',
-                'date' => '2019',
-                'genre' => 'Action'
-            ]
-        );
+        // DB::table('movies')->insert(
+        //     // [
+        //     //     'title' => 'ES',
+        //     //     'rating' => '8',
+        //     //     'date' => '2019',
+        //     //     'genre' => 'Action'
+        //     // ],
+        //     // [
+        //     //     'title' => 'ES',
+        //     //     'rating' => '8',
+        //     //     'date' => '2019',
+        //     //     'genre' => 'Action'
+        //     // ]
+        // );
     }
 }
